9bac1cd621f591074e4c67e93b5dbff224f0ba9e
Jeron Aldaron Lau <aldaronlau@gmail.com>
2025-02-09 23:55:43 -0600

    Add some code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,93 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Multimedia (Audio, Raster) Channel Newtypes and Conversions
+//!
+//! Each module is enabled with a feature by the same name.
+
+macro_rules! ops_int {
+    ($ty: ty) => {
+        impl core::ops::Add for $ty {
+            type Output = Self;
+
+            fn add(self, rhs: Self) -> Self {
+                Self(self.0.saturating_add(rhs.0))
+            }
+        }
+
+        impl core::ops::Sub for $ty {
+            type Output = Self;
+
+            fn sub(self, rhs: Self) -> Self {
+                Self(self.0.saturating_sub(rhs.0))
+            }
+        }
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+macro_rules! ops_float {
+    ($ty: ty) => {
+        impl core::ops::Add for $ty {
+            type Output = Self;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+            fn add(self, rhs: Self) -> Self {
+                Self(self.0 + rhs.0)
+            }
+        }
+
+        impl core::ops::Sub for $ty {
+            type Output = Self;
+
+            fn sub(self, rhs: Self) -> Self {
+                Self(self.0 - rhs.0)
+            }
+        }
+    };
+}
+
+#[cfg(feature = "signed")]
+pub mod signed {
+    //! Signed channel newtypes
+
+    /// 8-bit signed channel sample
+    pub struct Ch8(i8);
+    /// 12-bit signed channel sample
+    pub struct Ch12(i16);
+    /// 16-bit signed channel sample
+    pub struct Ch16(i16);
+    /// 24-bit signed channel sample
+    pub struct Ch24(i32);
+    /// 32-bit float (-1 to 1) channel sample
+    pub struct Ch32(f32);
+    /// 64-bit float (-1 to 1) channel sample
+    pub struct Ch64(f64);
+
+    ops_int!(Ch8);
+    ops_int!(Ch12);
+    ops_int!(Ch16);
+    ops_int!(Ch24);
+    ops_float!(Ch32);
+    ops_float!(Ch64);
+}
+
+#[cfg(feature = "unsigned")]
+pub mod unsigned {
+    //! Unsigned channel newtypes
+
+    /// 8-bit unsigned channel sample
+    pub struct Ch8(u8);
+    /// 12-bit unsigned channel sample
+    pub struct Ch12(u16);
+    /// 16-bit unsigned channel sample
+    pub struct Ch16(u16);
+    /// 24-bit unsigned channel sample
+    pub struct Ch24(u32);
+    /// 32-bit float (0 to 1) channel sample
+    pub struct Ch32(f32);
+    /// 64-bit float (0 to 1) channel sample
+    pub struct Ch64(f64);
+
+    ops_int!(Ch8);
+    ops_int!(Ch12);
+    ops_int!(Ch16);
+    ops_int!(Ch24);
+    ops_float!(Ch32);
+    ops_float!(Ch64);
 }
