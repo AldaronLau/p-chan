d61cda4076930d2757954a684180a1b836242b20
Jeron Aldaron Lau <aldaronlau@gmail.com>
2025-02-15 18:49:43 -0600

    Added new methods and constants

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,23 @@
 //!
 //! Each module is enabled with a feature by the same name.
 
+#[cfg(any(feature = "unsigned", feature = "signed"))]
 macro_rules! ops_int {
-    ($ty: ty) => {
+    ($ty: ty, $p: ty, $b: ty, $normalize: path) => {
+        impl $ty {
+            /// Minimum value
+            pub const MIN: Self = Self::new(<$p>::MIN);
+            /// Middle value
+            pub const MID: Self = Self::new(<$p>::MAX >> 1);
+            /// Maximum value
+            pub const MAX: Self = Self::new(<$p>::MAX);
+
+            /// Create a new channel value.
+            pub const fn new(value: $p) -> Self {
+                Self(value)
+            }
+        }
+
         impl core::ops::Add for $ty {
             type Output = Self;
 
@@ -24,8 +39,23 @@ macro_rules! ops_int {
     };
 }
 
+#[cfg(any(feature = "unsigned", feature = "signed"))]
 macro_rules! ops_float {
-    ($ty: ty) => {
+    ($ty: ty, $p: ty, $normalize: path, $min: literal, $mid: literal) => {
+        impl $ty {
+            /// Minimum value
+            pub const MIN: Self = Self::new($min);
+            /// Middle value
+            pub const MID: Self = Self::new($mid);
+            /// Maximum value
+            pub const MAX: Self = Self::new(1.0);
+
+            /// Create a new channel value.
+            pub const fn new(value: $p) -> Self {
+                Self(value)
+            }
+        }
+
         impl core::ops::Add for $ty {
             type Output = Self;
 
@@ -51,24 +81,49 @@ pub mod signed {
     //! Signed channel newtypes
 
     /// 8-bit signed channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch8(i8);
+   
     /// 12-bit signed channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch12(i16);
+   
     /// 16-bit signed channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch16(i16);
+   
     /// 24-bit signed channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch24(i32);
+   
     /// 32-bit float (-1 to 1) channel sample
+    #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default)]
+    #[repr(transparent)]
     pub struct Ch32(f32);
+   
     /// 64-bit float (-1 to 1) channel sample
+    #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default)]
+    #[repr(transparent)]
     pub struct Ch64(f64);
 
-    ops_int!(Ch8);
-    ops_int!(Ch12);
-    ops_int!(Ch16);
-    ops_int!(Ch24);
-    ops_float!(Ch32);
-    ops_float!(Ch64);
+    ops_int!(Ch8, i8, i16, core::convert::identity);
+    ops_int!(Ch12, i16, i32, core::convert::identity);
+    ops_int!(Ch16, i16, i32, core::convert::identity);
+    ops_int!(Ch24, i32, i64, core::convert::identity);
+    ops_float!(Ch32, f32, core::convert::identity, -1.0, 0.0);
+    ops_float!(Ch64, f64, core::convert::identity, -1.0, 0.0);
 }
 
 #[cfg(feature = "unsigned")]
@@ -76,22 +131,47 @@ pub mod unsigned {
     //! Unsigned channel newtypes
 
     /// 8-bit unsigned channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch8(u8);
+   
     /// 12-bit unsigned channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch12(u16);
+   
     /// 16-bit unsigned channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch16(u16);
+   
     /// 24-bit unsigned channel sample
+    #[derive(
+        Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default,
+    )]
+    #[repr(transparent)]
     pub struct Ch24(u32);
+   
     /// 32-bit float (0 to 1) channel sample
+    #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default)]
+    #[repr(transparent)]
     pub struct Ch32(f32);
+    
     /// 64-bit float (0 to 1) channel sample
+    #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default)]
+    #[repr(transparent)]
     pub struct Ch64(f64);
 
-    ops_int!(Ch8);
-    ops_int!(Ch12);
-    ops_int!(Ch16);
-    ops_int!(Ch24);
-    ops_float!(Ch32);
-    ops_float!(Ch64);
+    ops_int!(Ch8, u8, u16, core::convert::identity);
+    ops_int!(Ch12, u16, u32, core::convert::identity);
+    ops_int!(Ch16, u16, u32, core::convert::identity);
+    ops_int!(Ch24, u32, u64, core::convert::identity);
+    ops_float!(Ch32, f32, core::convert::identity, 0.0, 0.5);
+    ops_float!(Ch64, f64, core::convert::identity, 0.0, 0.5);
 }
